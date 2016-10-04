2e8262d589b95d5edd9d6c60edd011b471553dde
amir-zeldes <amir.zeldes@georgetown.edu>
2016-10-04 16:46:48 -0400

    Restructured directories for PyPI deployment

--- xrenner/xrenner.py
+++ xrenner/xrenner.py
@@ -13,7 +13,7 @@ from modules.xrenner_xrenner import Xrenner
 from glob import glob
 
 
-__version__ = "1.3.x"  # Develop
+__version__ = "1.4.0" 
 xrenner_version = "xrenner V" + __version__
 
 sys.dont_write_bytecode = True
