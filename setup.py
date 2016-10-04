4aebca7556c14fd7dfd54a4ead4f2d19fc52be11
amir-zeldes <amir.zeldes@georgetown.edu>
2016-10-04 18:51:45 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name = 'xrenner',
-  packages = find_packages() + [models],
+  packages = find_packages(),
   version = '1.4.1.5',
   description = 'A configurable, language independent coreferencer and (non) named entity recognizer',
   author = 'Amir Zeldes',
@@ -10,7 +10,7 @@ setup(
   package_data = {'':['models/*.*','README.rst','LICENSE.txt','doc/*.*','test/*.*','licenses/*.*']}
   url = 'https://github.com/amir-zeldes/xrenner', 
   license='Apache License, Version 2.0',
-  download_url = 'https://github.com/amir-zeldes/xrenner/tarball/1.4.1.5',
+  download_url = 'https://github.com/amir-zeldes/xrenner/tarball/v1.4.1.6',
   keywords = ['NLP', 'coreference', 'NER', 'named entity'],
   classifiers = ['Programming Language :: Python',
 'Programming Language :: Python :: 2',
