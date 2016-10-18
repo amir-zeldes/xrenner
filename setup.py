818b72caa7706002620735a86897720024d7d622
amir-zeldes <amir.zeldes@georgetown.edu>
2016-10-18 17:13:40 -0400

    Merge branch 'develop'
    # Conflicts:
    #	setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
   name = 'xrenner',
-  packages = find_packages() + [models],
-  version = '1.4.1.5',
+  packages = find_packages() + ['models'],
+  version = '1.4.1.6',
   description = 'A configurable, language independent coreferencer and (non) named entity recognizer',
   author = 'Amir Zeldes',
   author_email = 'amir.zeldes@georgetown.edu',
   package_data = {'':['models/*.*','README.rst','LICENSE.txt','doc/*.*','test/*.*','licenses/*.*']}
   url = 'https://github.com/amir-zeldes/xrenner', 
   license='Apache License, Version 2.0',
-  download_url = 'https://github.com/amir-zeldes/xrenner/tarball/1.4.1.5',
+  download_url = 'https://github.com/amir-zeldes/xrenner/tarball/1.4.1.6',
   keywords = ['NLP', 'coreference', 'NER', 'named entity'],
   classifiers = ['Programming Language :: Python',
 'Programming Language :: Python :: 2',
