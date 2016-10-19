bd57ebceb8ed5c0661ff0dc990d211ca3d6f2464
amir-zeldes <amir.zeldes@georgetown.edu>
2016-10-18 21:06:33 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 setup(
   name = 'xrenner',
   packages = find_packages() + ['models'],
-  version = '1.4.1.6',
+  version = '1.4.1.7',
   description = 'A configurable, language independent coreferencer and (non) named entity recognizer',
   author = 'Amir Zeldes',
   author_email = 'amir.zeldes@georgetown.edu',
-  package_data = {'':['models/*.*','README.rst','LICENSE.txt','doc/*.*','test/*.*','licenses/*.*']}
+  package_data = {'':['models/*.*','README.rst','LICENSE.txt','doc/*.*','test/*.*','licenses/*.*']},
   url = 'https://github.com/amir-zeldes/xrenner', 
   license='Apache License, Version 2.0',
-  download_url = 'https://github.com/amir-zeldes/xrenner/releases/tag/v1.4.1.6',
+  download_url = 'https://github.com/amir-zeldes/xrenner/releases/tag/v1.4.1.7',
   keywords = ['NLP', 'coreference', 'NER', 'named entity'],
   classifiers = ['Programming Language :: Python',
 'Programming Language :: Python :: 2',
