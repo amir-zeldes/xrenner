2122fc3de9dd6cf718d45cad45ac73fda5922ca9
amir-zeldes <amir.zeldes@georgetown.edu>
2018-01-18 10:27:15 -0500

    setup 2.0.0.0

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup, find_packages
 setup(
   name = 'xrenner',
   packages = find_packages(),
-  version = '1.4.1.8',
+  version = '2.0.0.0',
   description = 'A configurable, language independent coreferencer and (non) named entity recognizer',
   author = 'Amir Zeldes',
   author_email = 'amir.zeldes@georgetown.edu',
   package_data = {'':['README.rst','LICENSE.txt'],'xrenner':['models/*','test/*','licenses/*']},
+   install_requires=['scikit-learn'],
   url = 'https://github.com/amir-zeldes/xrenner',
   license='Apache License, Version 2.0',
   download_url = 'https://github.com/amir-zeldes/xrenner/releases/tag/v1.4.1.8',
