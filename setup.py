065c84292339e93afd39603fabdfbedcc70a8a48
amir-zeldes <amir.zeldes@georgetown.edu>
2020-02-27 14:08:51 -0500

    version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'xrenner',
   packages = find_packages(),
-  version = '2.1.0.2',
+  version = '2.1.0.3',
   description = 'A configurable, language independent coreferencer and (non) named entity recognizer',
   author = 'Amir Zeldes',
   author_email = 'amir.zeldes@georgetown.edu',
@@ -11,7 +11,7 @@ setup(
    install_requires=['scikit-learn==0.22.1','xgboost','flair','xmltodict'],
   url = 'https://github.com/amir-zeldes/xrenner',
   license='Apache License, Version 2.0',
-  download_url = 'https://github.com/amir-zeldes/xrenner/releases/tag/v2.1.0.2',
+  download_url = 'https://github.com/amir-zeldes/xrenner/releases/tag/v2.1.0.3',
   keywords = ['NLP', 'coreference', 'NER', 'named entity'],
   classifiers = ['Programming Language :: Python',
 'Programming Language :: Python :: 2',
